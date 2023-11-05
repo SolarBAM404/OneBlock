1527e5f1f1086a409a736d503e683e05491a30b6
Solar <solar+git@lunar-dev.com>
2023-11-05 00:58:35 +0000

    :tada: Initial Commit for OneBlock

---
+++ plugin/src/main/java/com/lunardev/oneblock/OneBlockPlugin.java
@@ -0,0 +1,17 @@
+package com.lunardev.oneblock;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class OneBlockPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
