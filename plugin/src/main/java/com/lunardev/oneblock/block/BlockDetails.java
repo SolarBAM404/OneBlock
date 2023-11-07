2536c240f6b714848015a815738109591e637ba7
Solar <solar+git@lunar-dev.com>
2023-11-07 02:09:24 +0000

    :sparkles: Added methods and fields for more access and details about the block/island

--- plugin/src/main/java/com/lunardev/oneblock/block/BlockDetails.java
+++ plugin/src/main/java/com/lunardev/oneblock/block/BlockDetails.java
@@ -3,56 +3,85 @@ package com.lunardev.oneblock.block;
 import lombok.Getter;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.block.Block;
 import org.bukkit.block.Chest;
 import org.bukkit.entity.Mob;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.util.Vector;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class BlockDetails {
 
     @Getter
+    private static final List<BlockDetails> blocks = new ArrayList<>();
+    @Getter
     private final Level currentLevel;
     @Getter
     private Location location;
+    @Getter
+    private OfflinePlayer owner;
+    @Getter
+    private Material currentBlock;
     private Random rand;
 
-    public BlockDetails(Level currentLevel, Location location) {
+    public BlockDetails(Level currentLevel, Location location, OfflinePlayer owner) {
         this.currentLevel = currentLevel;
         this.location = location;
+        this.owner = owner;
         rand = new Random((long) location.getBlockX() * location.getBlockZ());
+
+        BlockDetails.getBlocks().add(this);
+
+    }
+    
+    public static @Nullable BlockDetails getBlockLocation(Location location) {
+        for (BlockDetails block : blocks) {
+            if (location.getBlockZ() == block.location.getBlockZ()
+                    && location.getBlockX() == block.location.getBlockX()) {
+                return block;
+            }
+        }
+        return null;
     }
 
     public void spawnBlock(Material block) {
         location.getBlock().setType(block);
+        currentBlock = block;
     }
 
     public void spawnBlock(Chest chest) {
         location.getBlock().setType(chest.getType());
+        currentBlock = chest.getType();
         Chest chestBlock = (Chest) location.getBlock().getState();
         chestBlock.getInventory().setContents(chest.getInventory().getContents());
     }
 
     public void spawnBlock() {
+        Material material;
+
         if (rand.nextFloat(1) < 0.04) {
             ItemStack[] itemSet = currentLevel.getLootPool().getRandomItemSet(rand);
             Block block = location.getBlock();
             if (rand.nextBoolean()) {
-                block.setType(Material.BARREL);
+                material = Material.BARREL;
             } else {
-                block.setType(Material.CHEST);
+                material = Material.CHEST;
             }
+            block.setType(material);
 
             InventoryHolder inventoryHolder = (InventoryHolder) block.getBlockData();
             inventoryHolder.getInventory().setContents(itemSet);
 
         } else {
-            Material material = currentLevel.getRandomMaterial(rand);
+            material = currentLevel.getRandomMaterial(rand);
             location.getBlock().setType(material);
         }
+        currentBlock = material;
 
         if (rand.nextFloat(1) < 0.02) {
             Mob[] mobSet = currentLevel.getMobPool().getRandomMobSet(rand);
@@ -62,5 +91,9 @@ public class BlockDetails {
                 mob.spawnAt(loc);
             }
         }
+    }
+
+    public boolean isBlockLocation(Location location) {
+        return location.getBlockX() == this.location.getBlockX() && location.getBlockZ() == this.location.getBlockZ();
     }
 }
